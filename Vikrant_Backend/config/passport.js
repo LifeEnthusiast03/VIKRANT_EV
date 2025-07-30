5be14fcbde542eb0ca01badc75d740a129893e42
SOUGATA SAHA <sahasougata820@gmail.com>
2025-07-30 23:51:40 +0530

    change

--- Vikrant_Backend/config/passport.js
+++ Vikrant_Backend/config/passport.js
@@ -70,7 +70,9 @@ passport.use(new LocalStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.NODE_ENV === 'production' 
+    ? "https://vikrant-backend.vercel.app/auth/google/callback"
+    : "http://localhost:5000/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
    
