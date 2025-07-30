2fa9452f0d1267a01850539c5b8c5f1a9c43141d
SOUGATA SAHA <sahasougata820@gmail.com>
2025-07-31 00:00:10 +0530

    add

--- Vikrant_fronted/src/utils/constants.js
+++ Vikrant_fronted/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL =  'http://localhost:5000'
+export const API_BASE_URL =  'https://vercel.com/lifeenthusiast03s-projects/vikrant-backend'
 
 export const ROUTES = {
   HOME: '/',
